d4a6dcbe98280e1a1ebbcaf76f88be44e112ea8a
张磊 <62880914+zl-comment@users.noreply.github.com>
2025-01-04 15:57:14 +0800

    bert_core添加了本地模型

--- evaluation/bert_score/score.py
+++ evaluation/bert_score/score.py
@@ -14,7 +14,7 @@ from .utils import get_idf_dict, bert_cos_score_idf,\
 __all__ = ['score', 'plot_example']
 
 def score(cands, refs, bert="bert-base-multilingual-cased",
-          num_layers=8, verbose=False, no_idf=False, batch_size=64):
+          num_layers=8, verbose=True, no_idf=False, batch_size=64):
     """
     BERTScore metric.
     Args:
@@ -28,9 +28,13 @@ def score(cands, refs, bert="bert-base-multilingual-cased",
     """
     assert len(cands) == len(refs)
     assert bert in bert_types
-
-    tokenizer = BertTokenizer.from_pretrained(bert)
-    model = BertModel.from_pretrained(bert)
+    print(bert)
+    #加载本地模型
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    BERT_BASE_UNCASED = os.path.abspath(os.path.join(current_dir, '..\\..\\..\\google\\bert-base-uncased'))
+    print(BERT_BASE_UNCASED)
+    tokenizer = BertTokenizer.from_pretrained(BERT_BASE_UNCASED)
+    model = BertModel.from_pretrained(BERT_BASE_UNCASED)
     model.eval()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.to(device)
@@ -77,11 +81,13 @@ def plot_example(h, r, verbose=False, bert="bert-base-multilingual-cased",
         - :param: `num_layers` (int): the layer of representation to use
     """
     assert bert in bert_types
-
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    BERT_BASE_UNCASED = os.path.abspath(os.path.join(current_dir, '..\\..\\..\\google\\bert-base-uncased'))
+    print(BERT_BASE_UNCASED)
     if verbose:
         print('loading BERT model...')
-    tokenizer = BertTokenizer.from_pretrained(bert)
-    model = BertModel.from_pretrained(bert)
+    tokenizer = BertTokenizer.from_pretrained(BERT_BASE_UNCASED)
+    model = BertModel.from_pretrained(BERT_BASE_UNCASED)
     model.eval()
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model.to(device)
