8b0ffa4454ceb534943ce6072c16dfe927f0f03f
张磊 <62880914+zl-comment@users.noreply.github.com>
2025-01-08 21:48:25 +0800

    移动和创建

---
+++ outputs/Llama-2-7b-chat-hf/Llama-3.2-3B/attack_windows.sh
@@ -0,0 +1,31 @@
+python cold_decoding.py `
+    --seed 12 `
+    --mode "proxy" `
+    --pretrained_model "Llama-2-7b-chat-hf" `
+    --init-temp 1 `
+    --length 20 `
+    --max-length 20 `
+    --num-iters 2000 `
+    --min-iters 0 `
+    --goal-weight 100 `
+    --rej-weight 100 `
+    --stepsize 0.1 `
+    --kl_max_weight 0.1 `
+    --noise-iters 1 `
+    --win-anneal-iters 1000 `
+    --start 3 `
+    --end 50 `
+    --lr-nll-portion 1.0 `
+    --topk 10 `
+    --output-lgt-temp 1 `
+    --verbose `
+    --straight-through `
+    --large-noise-iters 50,200,500,1500 `
+    --large_gs_std 0.1,0.05,0.01,0.001 `
+    --stepsize-ratio 1 `
+    --batch-size 8 `
+    --print-every 1000 `
+    --fp16 `
+    --proxy_model "Llama-3.2-3B" `
+    --proxy_model_path "D:\ZLCODE\model\Llama-3.2-3B" `
+     --wandb
